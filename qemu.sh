e33465fabb2025b7bd85a91278b56f5aa26ffb1c
m1ckk <mick21@live.nl>
2021-05-31 22:32:36 +0200

    Changed qemu.sh which is able to boot Ubuntu now.

--- qemu.sh
+++ qemu.sh
@@ -1,1 +1,1 @@
-qemu-system-x86_64 -machine q35,smm=on -drive if=pflash,format=raw,file=./Build/OvmfX64/DEBUG_CLANGPDB/FV/OVMF.fd -m 4096m -global ICH9-LPC.disable_s3=1 -debugcon file:/tmp/debug.log -global isa-debugcon.iobase=0x402
+qemu-system-x86_64 -vga qxl -enable-kvm -machine q35,accel=kvm -cpu host -m 2G -global ICH9-LPC.disable_s3=1 -drive if=pflash,format=raw,unit=0,file=./Build/OvmfX64/DEBUG_CLANGPDB/FV/OVMF_CODE.fd,readonly=on -drive if=pflash,format=raw,file=./Build/OvmfX64/DEBUG_CLANGPDB/FV/OVMF_VARS.fd -drive file=/mnt/part5/qemu_drives/example.qcow2,format=qcow2,if=virtio
